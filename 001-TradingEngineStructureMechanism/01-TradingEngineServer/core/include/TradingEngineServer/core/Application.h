7601be4d7e70bb9a4ae6e13522809a5f2057c530
sakshammaggu <sakshammaggu80@gmail.com>
2025-09-21 18:14:57 +0530

    repo update

--- 001-TradingEngineStructureMechanism/01-TradingEngineServer/core/include/TradingEngineServer/core/Application.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-namespace TradingEngineServer::Core {
-    class Application {
-        public:
-            static int Run(int argc, char* argv[]);
-    };
-}
--- 001-TradingEngineStructureMechanism/01-TradingEngineServer/server/src/main.cpp
+++
@@ -1,5 +0,0 @@
-#include "TradingEngineServer/Core/Application.h"
-
-int main(int argc, char** argv) {
-    return TradingEngineServer::Core::Application::Run(argc, argv);
-}
