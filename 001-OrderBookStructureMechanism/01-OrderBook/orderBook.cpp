f7f4abf907365e2f7f8b346869f9a72e51c2afa1
sakshammaggu <sakshammaggu80@gmail.com>
2025-09-23 15:20:34 +0530

    Implement transaction logic in flipBalance for user fund and stock transfers

--- 001-OrderBookStructureMechanism/01-OrderBook/orderBook.cpp
+++ 001-OrderBookStructureMechanism/01-OrderBook/orderBook.cpp
@@ -9,7 +9,29 @@ int Order::orderCounterBid = 0; // Initialize the static counter for BID orders
 int Order::orderCounterAsk = 0; // Initialize the static counter for ASK orders
 
 void OrderBook::flipBalance(const std::string& userId1, const std::string& userId2, double quantity, double price) {
+    if (users.find(userId1) != users.end() && users.find(userId2) != users.end()) {
+        if (users[userId1].userBalance.balance["USD"] >= quantity * price) {
+            if (users[userId2].userBalance.balance[TICKER] >= quantity) {
+                users[userId1].userBalance.balance["USD"] -= quantity * price;
+                users[userId1].userBalance.balance[TICKER] += quantity;
 
+                users[userId2].userBalance.balance["USD"] += quantity * price;
+                users[userId2].userBalance.balance[TICKER] -= quantity;
+
+                cout << "Funds and stocks Transaction successful: " << userId1 << " bought " << quantity << " " << TICKER << " from " << userId2 << " at price " << price << endl;
+                return;
+            } else {
+                cout << "User " << userId2 << " does not have enough " << TICKER << " balance to complete the transaction." << endl;
+                return;
+            }
+        } else {
+            cout << "User " << userId1 << " does not have enough USD balance to complete the transaction." << endl;
+            return;
+        }
+    } else {
+        cout << "One or both users not found." << endl;
+        return;
+    }
 }
 
 std::string OrderBook::addBid(std::string Username, double Price, int Quantity) {
