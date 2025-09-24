cca791c7a448eb816d14ad9f744afef4b44e652c
sakshammaggu <sakshammaggu80@gmail.com>
2025-09-24 14:22:16 +0530

    Add initialization for multiple Market Makers users in OrderBook constructor

--- 001-OrderBookStructureMechanism/01-OrderBook/orderBook.cpp
+++ 001-OrderBookStructureMechanism/01-OrderBook/orderBook.cpp
@@ -34,6 +34,59 @@ void OrderBook::flipBalance(const std::string& userId1, const std::string& userI
     }
 }
 
+// Implementation of OrderBook constructor
+OrderBook::OrderBook() {
+    // User 1 with initial balance
+    Balance balance1("USD", 10000.0);
+    balance1.addBalances(TICKER, 1000.0);
+    User user1("MarketMaker1", balance1);
+    users[user1.userName] = user1;
+
+    cout << "Initialized OrderBook with Market Makers 1 and balance 1." << endl;
+
+    Order bid1(user1.userName, "BID", 110.0, 10);
+    Order ask1(user1.userName, "ASK", 115.0, 5);
+    Order bid2(user1.userName, "BID", 111.0, 8);
+    Order ask2(user1.userName, "ASK", 119.0, 12);
+
+    bids.push_back(bid1);
+    asks.push_back(ask1);
+    bids.push_back(bid2);
+    asks.push_back(ask2);
+
+    // User 2 with initial balance
+    Balance balance2("USD", 10000.0);
+    balance2.addBalances(TICKER, 2000.0);
+    User user2("MarketMaker2", balance2);
+    users[user2.userName] = user2;
+
+    cout << "Initialized OrderBook with Market Makers 2 and balance 2." << endl;
+
+    Order bid3(user2.userName, "bid", 109.0, 10);
+    Order ask3(user2.userName, "ask", 125.0, 5);
+    Order bid4(user2.userName, "bid", 112.0, 8);
+    Order ask4(user2.userName, "ask", 120.0, 12);
+
+    bids.push_back(bid3);
+    asks.push_back(ask3);
+    bids.push_back(bid4);
+    asks.push_back(ask4);
+
+    // User 3 with initial balance
+    Balance balance3("USD", 50000.0);
+    balance3.addBalances(TICKER, 0.0);
+    User user3("MarketMaker3", balance3);
+    users[user3.userName] = user3;
+
+    cout << "Initialized OrderBook with Market Makers 3 and balance 3." << endl;
+
+    Order bid5(user3.userName, "bid", 105, 10);
+    Order bid6(user3.userName, "bid", 108, 10);
+
+    bids.push_back(bid5);
+    bids.push_back(bid6);
+}
+
 std::string OrderBook::addBid(std::string Username, double Price, int Quantity) {
 
 }
